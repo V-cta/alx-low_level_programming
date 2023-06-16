fcd1b46942ce33f15176940a3ada1646da5f876f
V-cta <victor.ngugi.edu@gmail.com>
2023-06-15 17:03:28 -0700

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *Main - main entry point
- *Return : always 0 (success)
+ *main - main entry point
+ *return : always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
