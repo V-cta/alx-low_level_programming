9c10e420500396a721854f1d41da3336e07229b5
V-cta <victor.ngugi.edu@gmail.com>
2023-06-19 16:41:04 -0700

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ *main- entry point 
+ *Description: 'printing the alphabet in lowercase'
+ *Return; always 0
+ */
+int main(void)
+{
+	putchar(70);
+	putchar('\n');
+	return (0);
+}
