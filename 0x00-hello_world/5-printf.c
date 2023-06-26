5740b67ee99039cdf297669d91c3b461b9f35161
V-cta <victor.ngugi.edu@gmail.com>
2023-06-26 13:27:21 -0700

    Proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * return: always 0 (success)
+ *main - Entry point
+ *Description 'print f for proper grammar'
+ *Return: always 0 (success)
  */
 int main(void)
 {
